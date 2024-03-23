e2830fe0b23b9e6595bfd736b8660835e0ae6c0d
Fabio Henrique Farias da Silva <fabioh.farias@outlook.com>
2024-03-23 11:12:57 -0300

    update

--- java/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,13 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class KindergartenGarden {
 
+    List<String> names = List.of("Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry");
+
+    List<Plant> window1 = new ArrayList<>();
+    List<Plant> window2 = new ArrayList<>();
+
     KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        String[] gardens = garden.split("\n");
+
+        for (char c : gardens[0].toCharArray()) {
+            window1.add(Plant.getPlant(c));
+        }
+
+        for (char c : gardens[1].toCharArray()) {
+            window2.add(Plant.getPlant(c));
+        }
+
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Plant> plants = new ArrayList<>();
+
+        int index = names.indexOf(student);
+
+        plants.add(window1.get(index * 2));
+        plants.add(window1.get(index * 2 + 1));
+
+        plants.add(window2.get(index * 2));
+        plants.add(window2.get(index * 2 + 1));
+
+        return plants;
     }
 
 }
