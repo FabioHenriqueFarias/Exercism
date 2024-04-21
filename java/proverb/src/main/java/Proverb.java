080e64df2696354abed5cbb337bfa4a85fa5eb37
Fabio Henrique Farias da Silva <fabioh.farias@outlook.com>
2024-04-21 14:52:31 -0300

    update exercism

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -10,8 +10,13 @@ class Proverb {
         if (words.length == 0) {
             return "";
         }
-
         StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < words.length - 1; i++) {
+            result.append("For want of a ").append(words[i]).append(" the ").append(words[i + 1]).append(" was lost.\n");
+        }
+
+        result.append("And all for the want of a ").append(words[0]).append(".");
 
         return result.toString();
     }
---
+++ java/saddle-points/src/main/java/Matrix.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Set;
+
+class Matrix {
+
+    List<List<Integer>> values;
+
+    Matrix(List<List<Integer>> values) {
+        this.values = values;
+    }
+
+    Set<MatrixCoordinate> getSaddlePoints() {
+        Set<MatrixCoordinate> saddlePoints = new HashSet<>();
+
+//        for ()
+
+        return saddlePoints;
+    }
+}
